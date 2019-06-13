6ffa14309e99677d965eafce22909747e0b9cc44
singhsanket143 <singhsanket143@gmail.com>
2019-06-13 22:22:18 +0530

    Added cars class

---
+++ lib/car.rb
@@ -0,0 +1,10 @@
+class Car
+    #constructor
+    def initialize(reg_num, colour)
+          @reg_num = reg_num # Registration Number Of Car
+          @colour = colour # Color Of Car
+    end
+
+    attr_accessor :reg_num, :colour, :parking_slot
+end
+  
